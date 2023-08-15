55504ceb546eee4fabb730596182761948db070b
Mrekyle <>
2023-08-15 15:31:40 +0000

    Add admin dashboard funcionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
+
+"""
+By importing the moduls in to the admin file we are able to then add them into
+the admin dashboard for the admin to access and manipulate
+"""
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+# admin.site.register(Post)
+# admin.site.register(Comment)
