102442c65b2281b1b135df47512f68782a6f93e3
Mrekyle <>
2023-08-19 09:57:21 +0000

    Add class based views. Add base html templates for the blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    """
+    A class based view allows the individual views to inherit from each other
+    allowing us to have resusable code. Unlike in function based views.
+    """
+    model = Post
+    # Ordering the query by the date that the blog posts were created
+    # status=1 allows only the 'approved' posts to be displayed to the user
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # Saying what template to render all the blog posts onto 
+    template_name = 'index.html'
+    paginate_by = 6
